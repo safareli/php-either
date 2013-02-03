faa2a45960a90801ad6158d85cdf657aa3b85d62
Joseph Moniz <joseph.moniz@gmail.com>
2013-02-02 17:17:53 -0800

    created util companion object

---
+++ src/PlasmaConduit/either/EitherUtil.php
@@ -0,0 +1,42 @@
+<?php
+namespace PlasmaConduit;
+use PlasmaConduit\Map;
+use PlasmaConduit\either\Left;
+use PlasmaConduit\either\Right;
+use Exception;
+
+class EitherUtil {
+
+    public static function lefts($map) {
+        $map = self::_ensureMap($map);
+        return $map->filter(function($value) {
+            return $value instanceof Left;
+        });
+    }
+
+
+    public static function rights($map) {
+        $map = self::_ensureMap($map);
+        return $map->filter(function($value) {
+            return $value instanceof Right;
+        });
+    }
+
+    public static function partition($map) {
+        $map = self::_ensureMap($map);
+        return $map->partition(function($value) {
+            return $value instanceof Left;
+        });
+    }
+
+    private static function _ensureMap($map) {
+        if (is_array($map)) {
+            return new Map();
+        }
+        if (!($map instanceof Map)) {
+            throw new Exception("EitherUtil methods require map or array");
+        }
+        return $map
+    }
+
+}
