4b61c2d06d42238c6ba920fd00da7a9a44fc03fe
Joseph Moniz <joseph.moniz@gmail.com>
2013-03-22 20:43:28 -0700

    aliasing

--- spec/PlasmaConduit/either/EitherUtil.php
+++ spec/PlasmaConduit/either/EitherUtil.php
@@ -2,47 +2,47 @@
 namespace spec\PlasmaConduit\either;
 use PHPSpec2\ObjectBehavior;
 use PlasmaConduit\Map;
-use PlasmaConduit\either\Left;
-use PlasmaConduit\either\Right;
+use PlasmaConduit\either\Left as RLeft;
+use PlasmaConduit\either\Right as RRight;
 
 class EitherUtil extends ObjectBehavior {
 
     function it_should_filter_lefts_on_array_for_lefts() {
-        $eithers = [new Left(""), new Right(""), new Left("")];
+        $eithers = [new RLeft(""), new RRight(""), new RLeft("")];
         self::lefts($eithers)->length()->shouldReturn(2);
     }
 
     function it_should_filter_lefts_on_map_for_lefts() {
-        $eithers = new Map([new Left(""), new Right(""), new Left("")]);
+        $eithers = new Map([new RLeft(""), new RRight(""), new RLeft("")]);
         self::lefts($eithers)->length()->shouldReturn(2);
     }
 
     function it_should_throw_when_lefts_is_called_with_number() {
-        $this->shouldThrow()->duringLefts(123);
+        $this->shouldThrow()->duringRLefts(123);
     }
 
     function it_should_filter_rights_on_array_for_rights() {
-        $eithers = [new Left(""), new Right(""), new Left("")];
+        $eithers = [new RLeft(""), new RRight(""), new RLeft("")];
         self::rights($eithers)->length()->shouldReturn(1);
     }
 
     function it_should_filter_rights_on_map_for_rights() {
-        $eithers = new Map([new Left(""), new Right(""), new Left("")]);
+        $eithers = new Map([new RLeft(""), new RRight(""), new RLeft("")]);
         self::rights($eithers)->length()->shouldReturn(1);
     }
 
     function it_should_throw_when_rights_is_called_with_number() {
-        $this->shouldThrow()->duringRights(123);
+        $this->shouldThrow()->duringRRights(123);
     }
 
     function it_should_partition_for_on_array_partition() {
-        $eithers     = [new Left(""), new Right(""), new Left("")];
+        $eithers     = [new RLeft(""), new RRight(""), new RLeft("")];
         $partitioned = self::partition($eithers);
         $partitioned->get(0)->get()->length()->shouldReturn(2);
     }
 
     function it_should_partition_for_on_map_partition() {
-        $eithers = new Map([new Left(""), new Right(""), new Left("")]);
+        $eithers = new Map([new RLeft(""), new RRight(""), new RLeft("")]);
         $partitioned = self::partition($eithers);
         $partitioned->get(1)->get()->length()->shouldReturn(1);
     }
